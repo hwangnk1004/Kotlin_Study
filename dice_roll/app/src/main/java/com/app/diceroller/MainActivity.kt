e8c69c3feb2c104addce130396d888160f68a555
hwangnk1004 <topsexy_@naver.com>
2020-12-17 14:54:58 +0900

    Clean code

--- dice_roll/app/src/main/java/com/app/diceroller/MainActivity.kt
+++ dice_roll/app/src/main/java/com/app/diceroller/MainActivity.kt
@@ -8,12 +8,14 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var diceImage: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val rollbutton: Button = findViewById(R.id.roll_button)
+        diceImage = findViewById(R.id.dice_image)
 
         rollbutton.setOnClickListener {
             rollDice()
@@ -32,7 +34,7 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-        val diceImage: ImageView = findViewById(R.id.dice_image)
+
         diceImage.setImageResource(drawableResource)
 
     }
