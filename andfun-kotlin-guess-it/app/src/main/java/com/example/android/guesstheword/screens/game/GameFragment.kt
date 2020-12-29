412d62d4a7f4ad2ee7c8f3f885ac9237eb225ccf
hwangnk1004 <topsexy_@naver.com>
2020-12-29 22:25:25 +0900

    Study andorid architecture

---
+++ andfun-kotlin-guess-it/app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -0,0 +1,104 @@
+/*
+ * Copyright 2018, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.guesstheword.screens.game
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.NavHostFragment.findNavController
+import com.example.android.guesstheword.R
+import com.example.android.guesstheword.databinding.GameFragmentBinding
+import java.util.EnumSet.of
+
+/**
+ * Fragment where the game is played
+ */
+class GameFragment : Fragment() {
+
+    private lateinit var viewModel: GameViewModel
+    // The current word
+
+
+    private lateinit var binding: GameFragmentBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        // Inflate view and obtain an instance of the binding class
+        binding = DataBindingUtil.inflate(
+                inflater,
+                R.layout.game_fragment,
+                container,
+                false
+        )
+
+        /** ViewModel을 직접 구성하면 프래그먼트가 다시 생성 될 때마다 ViewModel을 생성하게 된다
+         *  회전 문제를 해결 할 수 없다
+         *  대신 생명주기 라이브러리가 ViewModel을 생성한다
+         *  ViewModelProvider라는 클래스에서 올바른 ViewModel을 제공한다. */
+        Log.i("GameFragment", "Called ViewModelProviders.of!")
+        viewModel = ViewModelProviders.of(this).get(GameViewModel::class.java)
+        /** 첫 번째, this 키워드를 사용하여 프래그먼트를 전달한다.
+         *  두 번째, 여기에서 원하는 특정 ViewModel 클래스를 전달한다. */
+
+
+        binding.correctButton.setOnClickListener {
+            viewModel.onCorrect()
+            updateWordText()
+        }
+        binding.skipButton.setOnClickListener {
+            viewModel.onSkip()
+            updateWordText()
+        }
+
+
+
+        viewModel.score.observe(this, Observer { newScore ->
+            binding.scoreText.text = newScore.toString()
+        })
+        updateWordText()
+        return binding.root
+
+    }
+
+
+    /**
+     * Called when the game is finished
+     */
+    private fun gameFinished() {
+        val action = GameFragmentDirections.actionGameToScore(viewModel.score.value ?: 0)
+
+        findNavController(this).navigate(action)
+    }
+
+
+    /** Methods for updating the UI **/
+
+    private fun updateWordText() {
+        binding.wordText.text = viewModel.word
+
+    }
+
+
+}
---
+++ andfun-kotlin-guess-it/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -0,0 +1,90 @@
+package com.example.android.guesstheword.screens.game
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GameViewModel : ViewModel() {
+    private var _word = MutableLiveData<String>()
+    val word: LiveData<String>
+        get() = _word
+
+    // The current score
+    /** LiveData를 캡슐화하여 다른곳에서 편집하는 것을 제한하기 위해 내부버전과 외부버전으로 나누어서 사용한다.
+     * 1) 외부버전은 LiveData타입을 사용하여 만든다.
+     * 2) 내부 MutableLiveData를 LiveData로 반환하는 외부 버전에 대한 백업 속성으로 만든다. */
+    private var _score = MutableLiveData<Int>()
+    val score: LiveData<Int>
+        get() = _score
+
+    /** 코틀린은 자동으로 필드에 대한 getter 및 setter를 만든다. */
+
+    // The list of words - the front of the list is the next word to guess
+    private lateinit var wordList: MutableList<String>
+
+    init {
+        Log.i("GameViewModel", "GameViewModel created!")
+        resetList()
+        nextWord()
+        _score.value = 0
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        Log.i("GameViewModel", "GameViewModel destroyed!!")
+    }
+
+    /**
+     * Resets the list of words and randomizes the order
+     */
+    private fun resetList() {
+        wordList = mutableListOf(
+                "queen",
+                "hospital",
+                "basketball",
+                "cat",
+                "change",
+                "snail",
+                "soup",
+                "calendar",
+                "sad",
+                "desk",
+                "guitar",
+                "home",
+                "railway",
+                "zebra",
+                "jelly",
+                "car",
+                "crow",
+                "trade",
+                "bag",
+                "roll",
+                "bubble"
+        )
+        wordList.shuffle()
+    }
+
+    /**
+     * Moves to the next word in the list
+     */
+    private fun nextWord() {
+        //Select and remove a word from the list
+        if (wordList.isEmpty()) {
+//            gameFinished()
+        } else {
+            _word.value = wordList.removeAt(0)
+        }
+    }
+
+    fun onSkip() {
+        _score.value = (score.value)?.minus(1)
+        nextWord()
+    }
+
+    fun onCorrect() {
+        _score.value = (score.value)?.plus(1)
+        nextWord()
+    }
+
+}
